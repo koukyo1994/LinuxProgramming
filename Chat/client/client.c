19c004e1c46219c43cabed35dcdf6a850ae334bb
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-07-24 17:11:22 +0900

    Need More work on client.c

--- Chat/client/client.c
+++ Chat/client/client.c
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 #define PORT 10080
 #define BUF_SIZE 1024
@@ -22,9 +23,57 @@ static void *send_msg(int *fd);
 
 int main(int argc, char *argv[]){
     int sock;
-    int i, n;
+    ssize_t n;
     char hn[64];
+    char buffer[BUF_SIZE], leave[BUF_SIZE];
+    struct sockaddr_in server;
     pthread_t tid;
 
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s host\n", argv[0]);
+        exit(1);
+    }
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        perror("socket");
+        exit(1);
+    }
+
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0){
+        perror("connect");
+        exit(1);
+    }
+    printf("\x1b[2J"); //Clear Console
+
+    input_name(sock);
+    if (read(sock, buffer, sizeof(buffer)) < 0){
+        perror("read");
+        exit(1);
+    }
+    buffer[strlen(buffer)-1] = '\0';
+    printf("%s\n", buffer);
+
+    pthread_create(&tid, NULL, (void *)&send_msg, (void *)&sock);
+    sprintf(leave, "%s left the room\a", hn);
+    leave[strlen(leave)-1] = '\0';
+
+    while (1){
+        buffer[0] = '\0';
+        n = read(sock, buffer, sizeof(buffer));
+        buffer[n] = '\0';
+        if (strcmp(buffer, leave) == 0){
+            break;
+        }
+        printf("%s\n", buffer);
+    }
+    pthread_join(tid, NULL);
+    close(sock);
+    return 0;
+}
+
+static int input_name(int fd){
 
 }
