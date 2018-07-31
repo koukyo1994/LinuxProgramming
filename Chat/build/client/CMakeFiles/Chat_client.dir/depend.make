c90af70ad2af5d3f6fc4c01606fd0ad65257b00a
koukyo1994 <arabiannight1994@yahoo.co.jp>
2018-07-31 19:29:06 +0900

    っｄ

---
+++ Chat/build/client/CMakeFiles/Chat_client.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Chat_client.
+# This may be replaced when dependencies are built.
---
+++ Chat/build/server/CMakeFiles/Chat_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chat_server.dir/server.c.o"
+  "Chat_server.pdb"
+  "Chat_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chat_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
